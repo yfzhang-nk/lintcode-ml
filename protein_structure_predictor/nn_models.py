1575b71ca449a5093cc59d49e2e69bd7565c4085
lanbijia <lbj_world@163.com>
2019-01-31 21:00:06 +0800

    ADD: more models

--- protein_structure_predictor/nn_models.py
+++ protein_structure_predictor/nn_models.py
@@ -2,9 +2,13 @@
 import os
 import numpy as np
 from keras import models
-from keras.layers import LSTM, Dense, TimeDistributed, Bidirectional, CuDNNLSTM, Embedding
+from keras.layers import (
+    Input, LSTM, Dense, TimeDistributed, Bidirectional, CuDNNLSTM, Embedding, RepeatVector,
+    Lambda, Permute, multiply, concatenate,
+)
 from keras.utils import to_categorical
-# from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.sequence import pad_sequences
+from keras import backend as K
 # from keras.optimizers import Adam, SGD
 
 
@@ -35,13 +39,17 @@ class BaseModel(object):
     def build_predict_model(self, *args, **kwargs):
         raise NotImplementedError
 
-    def load_model_weights(self, init_epoch=None):
-        if init_epoch and self._model:
+    def load_model_weights(self, init_epoch=None, model=None):
+        if init_epoch:
             weights_file = os.path.join(
                 self._model_dir,
                 self._file_pattern.format(epoch=init_epoch)
             )
-            self._model.load_weights(weights_file)
+            if model:
+                model.load_weights(weights_file, by_name=True)
+            elif self._model:
+                self._model.load_weights(weights_file)
+            print '===weights [{}] loaded==='.format(weights_file)
 
     def data_generator(self, *args, **kwargs):
         raise NotImplementedError
@@ -54,24 +62,94 @@ class BenchMarkModel(BaseModel):
             LSTM(
                 32, return_sequences=True,
                 input_shape=(None, 1),
-                dropout=0.1, # recurrent_dropout=0.5,
+                dropout=0.1,  # recurrent_dropout=0.5,
                 # activity_regularizer=keras.regularizers.l1(0.2)
             )
         )
         model.add(
             LSTM(
                 32, return_sequences=True,
-                dropout=0.1, # recurrent_dropout=0.5,
+                dropout=0.1,  # recurrent_dropout=0.5,
                 # activity_regularizer=keras.regularizers.l1(0.2)
             )
         )
         model.add(
             LSTM(
                 16, return_sequences=True,
-                dropout=0.1, # recurrent_dropout=0.5,
+                dropout=0.1,  # recurrent_dropout=0.5,
                 # activity_regularizer=keras.regularizers.l1(0.2)
             )
         )
+        model.add(TimeDistributed(Dense(self._output_size, activation='softmax')))
+        model.compile(
+            optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+        model.summary()
+        self._model = model
+        return model
+
+    def build_train_model(self):
+        return self._build_model()
+
+    def build_predict_model(self):
+        return self._build_model()
+
+    def data_generator(self, data_x, data_y):
+        pairs = [
+            ([xi / 30.0 for xi in x], to_categorical(
+                data_y[i], num_classes=self._output_size
+            )) for i, x in enumerate(data_x)
+        ]
+        while True:
+            np.random.shuffle(pairs)
+            for p in pairs:
+                x_len = len(p[0])
+                x = np.array(p[0]).reshape(1, x_len, self._input_size)
+                y = np.array(p[1]).reshape(1, x_len, self._output_size)
+                yield (x, y)
+
+
+class BidirectionModel(BaseModel):
+    # def _build_model(self):
+    #     model = models.Sequential()
+    #     model.add(Embedding(24, 16))
+    #     model.add(
+    #         Bidirectional(CuDNNLSTM(
+    #             16, return_sequences=True,
+    #         ))
+    #     )
+    #     model.add(
+    #         Bidirectional(CuDNNLSTM(
+    #             16, return_sequences=True, stateful=True
+    #         ))
+    #     )
+    #     model.add(Dense(64, activation='relu'))
+    #     model.add(TimeDistributed(Dense(self._output_size, activation='softmax')))
+    #     model.compile(
+    #         optimizer='adam',
+    #         loss='categorical_crossentropy',
+    #         metrics=['accuracy']
+    #     )
+    #     model.summary()
+    #     self._model = model
+    #     return model
+
+    def _build_model(self):
+        model = models.Sequential()
+        model.add(Embedding(24, 16, batch_input_shape=(1, None)))
+        model.add(
+            Bidirectional(CuDNNLSTM(
+                16, return_sequences=True,
+            ))
+        )
+        model.add(
+            Bidirectional(CuDNNLSTM(
+                16, return_sequences=True,
+            ))
+        )
+        model.add(Dense(64, activation='relu'))
         model.add(TimeDistributed(Dense(self._output_size, activation='softmax')))
         model.compile(
             optimizer='adam',
@@ -94,30 +172,137 @@ class BenchMarkModel(BaseModel):
                 data_y[i], num_classes=self._output_size
             )) for i, x in enumerate(data_x)
         ]
-    	while True:
-    	    np.random.shuffle(pairs)
-    	    for p in pairs:
-    	        x_len = len(p[0])
-    	        x = np.array(p[0]).reshape(1, x_len, self._input_size)
-    	        y = np.array(p[1]).reshape(1, x_len, self._output_size)
+        while True:
+            np.random.shuffle(pairs)
+            for p in pairs:
+                x_len = len(p[0])
+                x = np.array(p[0]).reshape(1, x_len)
+                y = np.array(p[1]).reshape(1, x_len, self._output_size)
                 yield (x, y)
 
 
-class BidirectionModel(BaseModel):
+class EncoderDecoderModel(BaseModel):
+    """refer to: https://github.com/keras-team/keras/blob/master/examples/lstm_seq2seq.py
+    """
+    def build_train_model(self):
+        latent_dim = 64
+        target_class_number = self._output_size + 1  # 包含start_token，总共4个类别
+        encoder_input = Input(shape=(None, ))
+        encoder = Embedding(24, 16, name='encoder_embedding')(encoder_input)
+        encoder = CuDNNLSTM(
+            latent_dim, return_sequences=True, name='encoder_lstm_1'
+        )(encoder)
+        encoder_outputs, state_h, state_c = CuDNNLSTM(
+            latent_dim, return_state=True, name='encoder_lstm_2'
+        )(encoder)
+        # We discard `encoder_outputs` and only keep the states.
+        encoder_states = [state_h, state_c]
+
+        decoder_input = Input(shape=(None, target_class_number))
+        decoder_lstm = CuDNNLSTM(
+            latent_dim, return_sequences=True, return_state=True, name='decoder_lstm_1'
+        )
+        decoder_output, _, _ = decoder_lstm(decoder_input, initial_state=encoder_states)
+        decoder_output = Dense(target_class_number, activation='softmax', name='decoder_softmax')(decoder_output)
+        model = models.Model([encoder_input, decoder_input], decoder_output)
+        model.compile(
+            optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+        model.summary()
+        self._model = model
+        return model
+
+    def build_predict_model(self):
+        latent_dim = 64
+        target_class_number = self._output_size + 1  # 包含start_token，总共4个类别
+        encoder_input = Input(shape=(None, ))
+        encoder = Embedding(24, 16, name='encoder_embedding')(encoder_input)
+        encoder = CuDNNLSTM(
+            latent_dim, return_sequences=True, name='encoder_lstm_1'
+        )(encoder)
+        encoder_outputs, state_h, state_c = CuDNNLSTM(
+            latent_dim, return_sequences=True, return_state=True, name='encoder_lstm_2'
+        )(encoder)
+        # We discard `encoder_outputs` and only keep the states.
+        encoder_states = [state_h, state_c]
+        encoder_model = models.Model(encoder_input, encoder_states)
+
+        decoder_input = Input(shape=(None, target_class_number))
+        decoder_state_input_h = Input(shape=(latent_dim,))
+        decoder_state_input_c = Input(shape=(latent_dim,))
+        decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+        decoder_lstm = CuDNNLSTM(
+            latent_dim, return_sequences=True, return_state=True, name='decoder_lstm_1'
+        )
+        decoder_output, state_h, state_c = decoder_lstm(decoder_input, initial_state=decoder_states_inputs)
+        decoder_states = [state_h, state_c]
+        decoder_output = Dense(target_class_number, activation='softmax', name='decoder_softmax')(decoder_output)
+        decoder_model = models.Model(
+            [decoder_input] + decoder_states_inputs,
+            [decoder_output] + decoder_states)
+        return encoder_model, decoder_model
+
+    def data_generator(self, data_x, data_y):
+        start_token = 3
+        pairs = [
+            (x, data_y[i]) for i, x in enumerate(data_x)
+        ]
+        while True:
+            np.random.shuffle(pairs)
+            for p in pairs:
+                x_len = len(p[0])
+                x = np.array(p[0]).reshape(1, x_len)
+                y = to_categorical([start_token] + p[1], num_classes=self._output_size + 1)
+                y = np.array(y).reshape(1, x_len + 1, self._output_size + 1)
+                yield [x, y[:, :-1, :]], y[:, 1:, :]
+
+    def predict(self, x_input, encoder_model, decoder_model):
+        start_token = 3
+        x_len = len(x_input)
+        x_in = np.array(x_input).reshape(1, x_len)
+        # encode the input as state vectors.
+        states_value = encoder_model.predict(x_in)
+        y_in = [start_token]
+        while len(y_in) < x_len + 1:
+            y = to_categorical(y_in, num_classes=self._output_size + 1)
+            y = np.array(y).reshape(1, len(y_in), self._output_size + 1)
+            y_out, h, c = decoder_model.predict([y] + states_value)
+            idx = np.argmax(y_out[0, -1, :])
+            y_in.append(idx)
+            # update states
+            states_value = [h, c]
+        print 'input x:', x_input, '\npredict y:', y_in[1:]
+        return y_in[1:]  # remove start_token
+
+
+class LatestEncoderDecoderModel(BaseModel):
+    """TODO: need to figure out why so poor performance"""
+    def __init__(self, *args, **kwargs):
+        self._maxlen = kwargs.pop('maxlen')
+        super(LatestEncoderDecoderModel, self).__init__(*args, **kwargs)
+
     def _build_model(self):
-        model = models.Sequential()
-        model.add(Embedding(24, 16))
-        model.add(
-            Bidirectional(CuDNNLSTM(
-                16, return_sequences=True,
-            ))
-        )
-        model.add(
-            Bidirectional(CuDNNLSTM(
-                16, return_sequences=True,
-            ))
-        )
-        model.add(TimeDistributed(Dense(self._output_size, activation='softmax')))
+        latent_dim = 32
+        target_class_number = self._output_size
+        encoder_input = Input(shape=(self._maxlen, ))
+        encoder = Embedding(26, 16)(encoder_input)
+        encoder = Bidirectional(CuDNNLSTM(
+            latent_dim / 2, return_sequences=True,
+        ))(encoder)
+        encoder = Bidirectional(CuDNNLSTM(
+            latent_dim / 2,
+        ))(encoder)
+        encoder = RepeatVector(self._maxlen)(encoder)
+        decoder = CuDNNLSTM(
+            latent_dim, return_sequences=True,
+        )(encoder)
+        decoder = CuDNNLSTM(
+            latent_dim, return_sequences=True,
+        )(decoder)
+        decoder_output = TimeDistributed(Dense(target_class_number, activation='softmax'))(decoder)
+        model = models.Model(encoder_input, decoder_output)
         model.compile(
             optimizer='adam',
             loss='categorical_crossentropy',
@@ -133,17 +318,114 @@ class BidirectionModel(BaseModel):
     def build_predict_model(self):
         return self._build_model()
 
-    def data_generator(self, data_x, data_y, batch_size=1):
+    def data_generator(self, data_x, data_y):
+        data_x = [np.array(x) + 1 for x in data_x]
+        x_in = pad_sequences(data_x, maxlen=self._maxlen, padding='post', value=0)
+        y_in = [to_categorical(y, num_classes=self._output_size) for y in data_y]
+        y_in = pad_sequences(y_in, maxlen=self._maxlen, padding='post', value=0)
         pairs = [
-            (x, to_categorical(
-                data_y[i], num_classes=self._output_size
-            )) for i, x in enumerate(data_x)
+            (x, y_in[i]) for i, x in enumerate(x_in)
         ]
-    	while True:
-    	    np.random.shuffle(pairs)
-    	    for p in pairs:
-    	        x_len = len(p[0])
-    	        x = np.array(p[0]).reshape(1, x_len)
-    	        y = np.array(p[1]).reshape(1, x_len, self._output_size)
+        while True:
+            np.random.shuffle(pairs)
+            for p in pairs:
+                x = np.array(p[0]).reshape(1, self._maxlen)
+                y = np.array(p[1]).reshape(1, self._maxlen, self._output_size)
                 yield (x, y)
 
+
+class AttentionEncoderDecoderModel(BaseModel):
+    """refer to: https://github.com/philipperemy/keras-attention-mechanism/blob/master/attention_lstm.py
+    """
+    def __init__(self, *args, **kwargs):
+        self._maxlen = kwargs.pop('maxlen')
+        super(AttentionEncoderDecoderModel, self).__init__(*args, **kwargs)
+
+    def attention_3d_block(self, q, v, single_attention_vector=True):
+    	# q.shape = (batch_size, time_steps, input_dim)
+    	input_dim = int(v.shape[2])
+        timestep_dim = int(v.shape[1])
+    	a = Permute((2, 1))(q)
+    	a = Dense(timestep_dim, activation='softmax', name='attention_dense')(a)
+    	if single_attention_vector:
+    	    a = Lambda(lambda x: K.mean(x, axis=1), name='dim_reduction')(a)
+    	    a = RepeatVector(input_dim)(a)
+    	a_probs = Permute((2, 1), name='attention_vec')(a)
+    	output_attention_mul = multiply([v, a_probs], name='attention_mul')
+        return output_attention_mul
+
+    def build_train_model(self):
+        latent_dim = 64
+        target_class_number = self._output_size + 1  # 包含start_token，总共4个类别
+        encoder_input = Input(shape=(self._maxlen, ))
+        encoder = Embedding(24, 16, name='encoder_embedding')(encoder_input)
+        encoder = CuDNNLSTM(
+            latent_dim, return_sequences=True, name='encoder_lstm_1'
+        )(encoder)
+        encoder_output = CuDNNLSTM(
+            latent_dim, return_sequences=True, name='encoder_lstm_2'
+        )(encoder)
+
+        decoder_input = Input(shape=(self._maxlen, target_class_number))
+        decoder_lstm = CuDNNLSTM(
+            latent_dim, return_sequences=True, name='decoder_lstm_1'
+        )
+        decoder_output = decoder_lstm(decoder_input)
+
+        # encoder attention after LSTM
+        attention_output = self.attention_3d_block(q=decoder_input, v=encoder_output)
+        print attention_output.shape, decoder_output.shape
+        merged_output = concatenate([attention_output, decoder_output])
+
+        decoder_output = TimeDistributed(
+            Dense(target_class_number, activation='softmax', name='decoder_softmax')
+        )(merged_output)
+        model = models.Model([encoder_input, decoder_input], decoder_output)
+        model.compile(
+            optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+        model.summary()
+        self._model = model
+        return model
+
+    def build_predict_model(self):
+        return self.build_train_model()
+
+    def data_generator(self, data_x, data_y):
+        start_token = 3
+        data_x = [np.array(x) + 1 for x in data_x]
+        x_in = pad_sequences(data_x, maxlen=self._maxlen, padding='post', value=0)
+        y_in = [to_categorical([start_token] + y, num_classes=self._output_size + 1) for y in data_y]
+        y_in = pad_sequences(y_in, maxlen=self._maxlen + 1, padding='post', value=0)
+        pairs = [
+            (x, y_in[i]) for i, x in enumerate(x_in)
+        ]
+        while True:
+            np.random.shuffle(pairs)
+            for p in pairs:
+                x = np.array(p[0]).reshape(1, self._maxlen)
+                y = np.array(p[1]).reshape(1, self._maxlen + 1, self._output_size + 1)
+                yield [x, y[:, :-1, :]], y[:, 1:, :]
+
+    def predict(self, x_input, y_output=None):
+        assert(self._model)
+        start_token = 3
+        x_len = len(x_input)
+        x_in = np.array(x_input).reshape(1, x_len)
+        x_in = pad_sequences(x_in, maxlen=self._maxlen, padding='post', value=0)
+        # encode the input as state vectors.
+        y_in = [start_token]
+        while len(y_in) < x_len + 1:
+            y = to_categorical(y_in, num_classes=self._output_size + 1)
+            y = np.array(y).reshape(1, len(y_in), self._output_size + 1)
+            y = pad_sequences(y, maxlen=self._maxlen, padding='post', value=0)
+            y_out = self._model.predict([x_in, y])
+            idx = np.argmax(y_out[0, len(y_in) - 1, :])
+            y_in.append(idx)
+        print 'input x:', x_input
+        print 'predict y:', y_in[1:]
+        if y_output:
+            print 'truth y:', y_output
+        return y_in[1:]  # remove start_token
--- protein_structure_predictor/protein_structure.py
+++ protein_structure_predictor/protein_structure.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 import numpy as np
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, Callback
 
-from nn_models import BenchMarkModel, BidirectionModel
+from nn_models import (
+    BenchMarkModel, BidirectionModel, EncoderDecoderModel,
+    LatestEncoderDecoderModel, AttentionEncoderDecoderModel,
+)
 
-SCALE_MAX = 1.0
 AMIDOGEN = [
     'A',
     'R',
@@ -25,20 +27,13 @@ AMIDOGEN = [
     'T',
     'W',
     'Y',
+    'U',
     'V',
     'X',
     'Y',
     'Z',
 ]
-AMIDOGEN_SIZE = len(AMIDOGEN)
-AMIDOGEN_MAPPING = dict(zip(AMIDOGEN, [idx + 1 for idx in range(AMIDOGEN_SIZE)]))
-
 SECSTR = ['H', 'E', 'C']
-SECSTR_MAPPING = {
-    'H': 0,
-    'E': 1,
-    'C': 2,
-}
 
 INPUT_SIZE = 1
 OUTPUT_SIZE = 3
@@ -56,13 +51,24 @@ def read_data(filename):
     return features, labels
 
 
-def preprocess_data(features, labels):
+def read_test_data(filename):
+    features = []
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        for idx, val in enumerate(lines):
+            val = val.strip()
+            if idx % 2 == 1:
+                features.append(val)
+    return features
+
+
+def preprocess_data(features, labels=[]):
     x = []
     y = []
     for f in features:
-        x.append([AMIDOGEN_MAPPING.get(s, 0) for s in f])
+        x.append([AMIDOGEN.index(s) for s in f])
     for l in labels:
-        y.append([SECSTR_MAPPING[s] for s in l])
+        y.append([SECSTR.index(s) for s in l])
     return x, y
 
 
@@ -72,9 +78,9 @@ def sample_test(filename, m, init_epoch=None):
     # preprocess data to [features, labels]
     x_in, y_in = preprocess_data(features, labels)
     print(x_in[0], y_in[0])
-    train_model = m.build_train_model()
+    m.build_train_model()
     if init_epoch:
-        train_model.load_weights(init_epoch=init_epoch)
+        m.load_model_weights(init_epoch=init_epoch)
     m.fit_generator(
         generator=m.data_generator(x_in, y_in), epochs=1, steps_per_epoch=1,
         validation_data=m.data_generator(x_in, y_in), validation_steps=1,
@@ -85,7 +91,18 @@ def sample_test(filename, m, init_epoch=None):
     print(m.predict(test_x), test_y)
 
 
-def train(filename, m, init_epoch=None, valid_split=0.1):
+class Evaluate(Callback):
+    def __init__(self, model, valid_x, valid_y):
+        self._model = model
+        self._valid_x, self._valid_y = valid_x, valid_y
+
+    def on_epoch_end(self, epoch, logs=None):
+        if hasattr(self._model, 'predict'):
+            for idx, x in enumerate(self._valid_x):
+                self._model.predict(x, self._valid_y[idx])
+
+
+def train(filename, m, init_epoch=None, valid_split=0.1, epochs=100):
     # read data from file
     features, labels = read_data(filename)
     # preprocess data to [features, labels]
@@ -97,36 +114,74 @@ def train(filename, m, init_epoch=None, valid_split=0.1):
     train_size = len(train_x_in)
     valid_size = len(valid_x_in)
     train_model = m.build_train_model()
+    evaluation_callback = Evaluate(m, x_in[-2:], y_in[-2:])
     checkpoint_callback = ModelCheckpoint(m.checkpoint_path())
     train_generator = m.data_generator(train_x_in, train_y_in)
     valid_generator = m.data_generator(valid_x_in, valid_y_in)
     # start training
     if init_epoch:
-        train_model.load_model_weights(init_epoch=init_epoch)
+        m.load_model_weights(init_epoch=init_epoch)
         train_model.fit_generator(
             generator=train_generator,
-            epochs=100,
+            epochs=epochs,
             steps_per_epoch=train_size,
             validation_data=valid_generator,
             validation_steps=valid_size,
-            callbacks=[checkpoint_callback],
+            callbacks=[checkpoint_callback, evaluation_callback],
             initial_epoch=init_epoch,
         )
     else:
         train_model.fit_generator(
             generator=train_generator,
-            epochs=100,
+            epochs=epochs,
             steps_per_epoch=train_size,
             validation_data=valid_generator,
             validation_steps=valid_size,
-            callbacks=[checkpoint_callback],
+            callbacks=[checkpoint_callback, evaluation_callback],
         )
+
+
+def predict(in_file, m, init_epoch, out_file):
+    # read data from file
+    features = read_test_data(in_file)
+    # preprocess data to features
+    x_in, _ = preprocess_data(features)
+    encoder_model, decoder_model = m.build_predict_model()
+    m.load_model_weights(init_epoch=init_epoch, model=encoder_model)
+    m.load_model_weights(init_epoch=init_epoch, model=decoder_model)
+    y_out = []
+    for x in x_in:
+        y_out.extend(m.predict(x, encoder_model, decoder_model))
+    with open(out_file, 'w') as f:
+        f.write('class\n')
+        for y in y_out:
+            f.write('{}\n'.format(y))
+    return y_out
+
+
+def predict_ex(in_file, m, init_epoch, out_file):
+    # read data from file
+    features = read_test_data(in_file)
+    # preprocess data to features
+    x_in, _ = preprocess_data(features)
+    model = m.build_predict_model()
+    m.load_model_weights(init_epoch=init_epoch, model=model)
+    y_out = []
+    for x in x_in:
+        y_out.extend(m.predict(x, model))
+    with open(out_file, 'w') as f:
+        f.write('class\n')
+        for y in y_out:
+            f.write('{}\n'.format(y))
+    return y_out
 
 
 if __name__ == "__main__":
     # m = BenchMarkModel('benchmark')
-    m = BidirectionModel('bidirection')
-    train(
-        "ss100_train.txt", m,
-    )
+    # m = BidirectionModel('bidirection-2')
+    # m = EncoderDecoderModel('encoder-decoder')
+    # m = LatestEncoderDecoderModel('latest-encoder-decoder', maxlen=100)
+    m = AttentionEncoderDecoderModel('attention-encoder-decoder', maxlen=100)
+    train("ss100_train.txt", m, init_epoch=None)
     # sample_test('ss100_train.txt', m)
+    # predict('ss100_test.txt', m, 100, 'encoder-decoder_out.csv')
